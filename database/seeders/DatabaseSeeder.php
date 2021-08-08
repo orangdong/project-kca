a2c259fe27a3e14d04d24a7c931d76cad69415a6
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-08-08 15:22:25 +0700

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Toko;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $toko = [
+            'name' => 'Toko A',
+            'lokasi' => 'Jl. Zilong Autowin',
+            'phone' => 12345
+        ];
+        $user = [
+            'toko_id' => 1,
+            'name' => 'Admin',
+            'username' => 'admin',
+            'password' => Hash::make('zilonglayla515'),
+            'role' => 'admin'
+        ];
+
+        Toko::create($toko);
+        User::create($user);
     }
 }
